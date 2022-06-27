f4bfe19cb5207c02b2c18d7496a17c6d6eefd8a5
Burak Kağan Özkan <ozkankaan1996@hotmail.com>
2022-06-28 00:43:42 +0300

    Update firebaseConfig.js

--- src/views/Upload/_firebaseConfig/firebaseConfig.js
+++ src/views/Upload/_firebaseConfig/firebaseConfig.js
@@ -2,13 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDi68go0LQNTvX7CKe6RoIRRczHdiifgD0",
-  authDomain: "bekaoz.firebaseapp.com",
-  projectId: "bekaoz",
-  storageBucket: "bekaoz.appspot.com",
-  messagingSenderId: "1093813577601",
-  appId: "1:1093813577601:web:212fa953a2a05858402a80",
-  measurementId: "G-SWY9CQFVFK",
+  
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
